ab4687ce3fe93d011d5e491114dfbc65ac50b513
egorusegorus <tomaszokraszewski@gmail.com>
2024-08-27 22:35:25 +0200

    1/2 funktioniert

--- Program.cs
+++ Program.cs
@@ -1,2 +1,85 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Let’s guess!");
+        Console.WriteLine("");
+        wer();
+    }
+
+    public static bool menu() {
+        int wahl = -1;
+        while ((wahl < 0) || (wahl > 2)) {
+            Console.WriteLine("Wer rät?");
+            Console.WriteLine("0. Ich.");
+            Console.WriteLine("1. Du.");
+            Console.WriteLine("2. Ausgang.");
+            string? str = Console.ReadLine();
+
+            try {
+                wahl = Convert.ToInt32(str);
+            } catch (FormatException) {
+                Console.WriteLine("Ungültige Eingabe. Bitte geben Sie eine Zahl ein.");
+                wahl = -1;  
+            }
+
+            switch (wahl) {
+                case 0:
+                    return true;
+                case 1:
+                    return false;
+                case 2:
+                    Environment.Exit(0);
+                    break;  
+                default:
+                    Console.WriteLine("Ungültige Auswahl. Bitte wählen Sie erneut.");
+                    break;
+            }
+        }
+        return false; 
+    }
+
+    public static void wer(){
+        bool werspielt = menu(); 
+    
+        if (werspielt) {
+            // hier ich
+        } else {
+            Console.Clear();
+            Console.WriteLine("Bitte denken Sie an eine Zahl zwischen 0 und 99 und behalten Sie sie im Kopf.");
+            Console.ReadLine();
+            Console.WriteLine("Drücken Sie bitte die Taste 'G', wenn Ihre Zahl größer ist, und die Taste 'K', wenn sie kleiner ist.");
+            Console.WriteLine("Wenn ich die Zahl erraten habe, drücken Sie bitte !");
+        }
+        derZahl();
+    }
+
+    public static int derZahl(){
+        int low = 0;
+        int high = 99;
+        int mid;
+
+        while(low <= high){
+            mid = (low + high) / 2;
+            Console.WriteLine("Ist deine Zahl: " + mid + "? (Bitte geben Sie 'g', 'k' oder '!')");
+            string? gOderK = Console.ReadLine();
+
+            while(gOderK != "g" && gOderK != "k" && gOderK != "!"){
+                Console.WriteLine("Etwas ist schief, bitte geben Sie 'g', 'k' oder '!':");
+                gOderK = Console.ReadLine();
+            }
+
+            if (gOderK == "g"){
+                low = mid + 1;
+            } else if (gOderK == "k"){
+                high = mid - 1;
+            } else {
+                Console.WriteLine("Ich habe die Zahl erraten!");
+                break;
+            }
+        }
+        return 0;
+    }
+}
